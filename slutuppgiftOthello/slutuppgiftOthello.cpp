3de569a3f4e6674145eef101c44130a189f0bc9b
Hugo Dunmark <hugo@dunis.nu>
2025-02-21 13:36:59 +0100

    mer planering

--- slutuppgiftOthello/slutuppgiftOthello.cpp
+++ slutuppgiftOthello/slutuppgiftOthello.cpp
@@ -1,5 +1,61 @@
 #include <iostream>
 #include <locale>
+#include <vector>
+
+struct GameSettings
+{
+	bool isPlayingAgainstComputer;
+	bool isComputerBlack = false;
+	int computerDifficulty = 0;
+};
+
+struct GameCoordinate
+{
+	int x;
+	int y;
+};
+
+struct Board
+{
+	bool isBlacksTurn;
+	int numberOfBlackDiscs;
+	int numberOfWhiteDiscs;
+	//positioner
+};
+
+std::vector<GameCoordinate> searchPossibleMoves(Board &board) {
+	std::vector<GameCoordinate> listOfPossibleMoves;
+	return listOfPossibleMoves;
+}
+
+bool placeDisc(Board& board, GameCoordinate) {
+	return false;
+}
+
+void displayBoard(Board& board, std::vector<GameCoordinate> = {}) {
+
+}
+
+GameCoordinate getPlayerInput(bool isBlacksTurn) {
+	return { 3,5 };
+}
+
+//[6]
+void initGameSettings(GameSettings& settings) {
+
+}
+
+//[7]
+bool endGame(Board& board) {
+	return false;
+}
+
+//[8]
+GameCoordinate chooseComputerMove(Board& board, std::vector<GameCoordinate>, int computerDifficulty) {
+
+}
+
+
 
 int main() {
 	setlocale(LC_ALL, "sv_SE");
