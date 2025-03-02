1755418250172646f854e868e56a1045b63c34d5
Hugo <hugo@dunis.nu>
2025-03-02 20:01:29 +0100

    -placera bricka (primitiv) -kolla om drag är möjligt -skriv ut overlay över möjliga drag

--- slutuppgiftOthello/slutuppgiftOthello.cpp
+++ slutuppgiftOthello/slutuppgiftOthello.cpp
@@ -4,6 +4,9 @@
 #include <vector>
 #include <string>
 #include <cctype>
+#include <algorithm>
+
+typedef signed char int8;
 
 
 struct GameSettings
@@ -18,98 +21,217 @@ struct GameSettings
 	}
 };
 
-struct GameCoordinate
+struct GameCoordinates
 {
-	int x;
+	static const int8 translations[8][2];
 	int y;
+	int x;
+	bool isInValid() {
+		if (x < 0 || x > 7 || y < 0 || y > 7) return true;
+		return false;
+	}
 };
+const int8 GameCoordinates::translations[8][2] = {
+	  {-1,-1},{-1,0 },{-1,1 },{0,-1 },{0,1 },{1,-1 },{1,0 },{1,1 } };
 
 struct Board
 {
 	bool isBlacksTurn;
-	int numberOfBlackDiscs,numberOfWhiteDiscs;
-	char boardDiscs[8][8] = {0}; //a tom, b svart, c vit
-	bool boardAdjacents[8][8] = {0}; //är ruta bredvid en ruta med bricka i.
+	int numberOfDiscs[2] = { 0, 0 }; //{antal svarta brickor, antal vita brickor}
+	char discs[8][8] = {0}; //a tom, b svart, c vit
+	//[y][x] gäller för alla arrayer som håller positioner
+	bool adjacents[8][8] = {0}; //är ruta bredvid en ruta med bricka i.
 
 	Board() {
-		isBlacksTurn = true;
-		numberOfBlackDiscs = numberOfWhiteDiscs = 2;
+		//den här funktionen körs när ett objekt av typ (klass) Board skapas
+
+		isBlacksTurn = true; //svart börjar alltid
+
+		//antal svarta brickor = antal vita brickor = 2
+		numberOfDiscs[0] = numberOfDiscs[1] = 2;
+
+		//iterera alla rutor och sätt deras status till tom ('a')
 		for (int i = 0; i < 8; ++i) {
 			for (int j = 0; j < 8; ++j) {
-				boardDiscs[i][j] = 'a';
+				discs[i][j] = 'a';
 			}
 		}
-		boardDiscs[3][3] = 'c';
-		boardDiscs[3][4] = 'b';
-		boardDiscs[4][3] = 'b';
-		boardDiscs[4][4] = 'c';
-		boardAdjacents[2][3] = true;
-		boardAdjacents[3][2] = true;
-		boardAdjacents[3][5] = true;
-		boardAdjacents[2][4] = true;
-		boardAdjacents[4][2] = true;
-		boardAdjacents[4][5] = true;
-		boardAdjacents[5][3] = true;
-		boardAdjacents[5][4] = true;
+		//placera startbrickorna      ...
+		discs[3][3] = 'c'; //... a a ...
+		discs[3][4] = 'b'; //..a c b a..
+		discs[4][3] = 'b'; //..a b c a..
+		discs[4][4] = 'c'; //... a a ...
+		                        //    ...
+		//specifiera de rutor som är bredvid en ruta med en bricka i
+		adjacents[2][2] = true;
+		adjacents[2][3] = true;//    ...
+		adjacents[2][4] = true;//..1 1 1 1..
+		adjacents[2][5] = true;//..1 0 0 1..
+		adjacents[3][2] = true;//..1 0 0 1..
+		adjacents[3][5] = true;//..1 1 1 1..
+		adjacents[4][2] = true;//    ...
+		adjacents[4][5] = true;
+		adjacents[5][2] = true;
+		adjacents[5][3] = true;
+		adjacents[5][4] = true;
+		adjacents[5][5] = true;
 	}
 };
 
-std::vector<GameCoordinate> searchPossibleMoves(Board &board) {
-	std::vector<GameCoordinate> listOfPossibleMoves;
-	return listOfPossibleMoves;
-}
+//[2.1]
+bool isValidMove(Board & board, GameCoordinates move) {
+	//relativ färg till färg på det drag som kollas
+	char sameColor = board.isBlacksTurn ? 'b' : 'c';
+	char otherColor = board.isBlacksTurn ? 'c' : 'b';
 
-bool placeDisc(Board& board, GameCoordinate) {
+	//iterera alla riktningar
+	for (int i = 0; i < 8; ++i) {
+		//gå ett steg i riktningen
+		GameCoordinates currCoords = { move.y + GameCoordinates::translations[i][0],
+			move.x + GameCoordinates::translations[i][1] };
+
+		//gå till nästa riktning om vi är utanför spelplanen
+		if (currCoords.isInValid()) continue;
+		//eller om INTE motsatt färg (alltså tom eller samma)
+		if (board.discs[currCoords.y][currCoords.x] != otherColor) continue;
+
+		/*har vi kommit så här långt har vi en bricka med motsatt färg
+		för att utföra ett drag krävs också en bricka med samma färg i 
+		slutet av kedjan med brickor i motsatt färg. 
+		*/
+		while (true) {
+			//gå ett steg i riktningen
+			currCoords = { currCoords.y + GameCoordinates::translations[i][0],
+			currCoords.x + GameCoordinates::translations[i][1] };
+
+			//fortsätt till nästa riktning om vi är utanför spelplanen
+			if (currCoords.isInValid()) break;
+			//eller på en tom ruta
+			if (board.discs[currCoords.y][currCoords.x] == 'a') break;
+
+			//hittar vi däremot en bricka med samma färg är draget giltigt
+			if (board.discs[currCoords.y][currCoords.x] == sameColor) return true;
+
+			//om det är motsatt färg fortsätter vi helt enkelt
+		}
+		
+	}
+	/*om vi har kollat alla riktningar utan att förutsättningarna för
+	ett giltigt drag mötts*/
 	return false;
 }
 
-void displayBoard(Board& board, GameSettings &settings ,std::vector<GameCoordinate> = {}) {
-	std::string emptySymbol = " .", blackSymbol = " S", whiteSymbol = " V";
-	std::cout << "\033[H\n  a b c d e f g h\n";
+//[2]
+std::vector<GameCoordinates> getListOfPossibleMoves(Board &board) {
+	std::vector<GameCoordinates> listOfPossibleMoves;
+	for (int y = 0; y < 8; ++y) {
+		for (int x = 0; x < 8; ++x) {
+			if (!board.adjacents[y][x]) continue;
+			if (isValidMove(board, { y,x })) listOfPossibleMoves.push_back({ y,x });
+		}
+	}
+	return listOfPossibleMoves;
+}
+
+//[3]
+bool placeDisc(Board& board, GameCoordinates coords) {
+	board.discs[coords.y][coords.x] = board.isBlacksTurn ? 'b' : 'c';
+	board.isBlacksTurn = board.isBlacksTurn ? false : true;
+	board.numberOfDiscs[board.isBlacksTurn ? 0 : 1] += 1;
+
+	board.adjacents[coords.y][coords.x] = false;
+	for (int i = 0; i < 8; ++i) {
+		GameCoordinates adjCoords = { coords.y + GameCoordinates::translations[i][0], 
+			coords.x + GameCoordinates::translations[i][1] };
+		if (adjCoords.isInValid()) continue;
+		if (board.discs[adjCoords.y][adjCoords.x] > 'a') continue;
+		board.adjacents[adjCoords.y][adjCoords.x] = true;
+	}
+
+	return true;
+}
+
+//[4]
+void displayBoard(Board& board, GameSettings &settings ,std::vector<GameCoordinates> movesOverlay = {}) {
+	//symboler som ska skrivas ut
+	std::string emptySymbol = " .", blackSymbol = " S", whiteSymbol = " V", possibleSymbol = " *";
+
+	//skriv ut brädets norra kant
+	std::cout << "\033[2J\033[H\n  a b c d e f g h\n";
+
+	//håll koll på vart vi är i listan över möjliga drag
+	int overlayCounter = 0; 
+	//iterera alla rader
 	for (int rowNumber = 0; rowNumber < 8; ++rowNumber) {
-		std::cout << rowNumber + 1;
-		char* rowArray = board.boardDiscs[rowNumber];
+
+		std::cout << rowNumber + 1; //skriv ut radnummret (+1 för 1-8 istället för 0-7)
+
+		char* rowArray = board.discs[rowNumber]; //spara alla rutor på raden i en temp-variabel
+
+		//iterera genom raden
 		for (int columnNumber = 0; columnNumber < 8; ++columnNumber) {
+			//OVERLAY AV MÖJLIGA DRAG
+			//vi förutsätter att listan över möjliga drag är sorterad
+			//om rutan vi är på är ett möjligt drag
+			if (movesOverlay[overlayCounter].y == rowNumber && movesOverlay[overlayCounter].x == columnNumber) {
+				//inkrementera räknaren men inte om vi har nått slutet på listan
+				overlayCounter += overlayCounter<movesOverlay.size()-1 ? 1 : 0;
+				//skriv ut symbolen som representerar ett möjligt drag
+				std::cout << possibleSymbol;
+				//gå direkt till nästa ruta (vi vill ju inte skriva ut dubbelt)
+				continue;
+			}
+
+			//kolla vad rutan har för status
 			switch (rowArray[columnNumber])
 			{
-			case 'a':
+			case 'a': //om tom
 				std::cout << emptySymbol;
 				break;
-			case 'b':
+			case 'b': //om svart
 				std::cout << blackSymbol;
 				break;
-			case 'c':
+			case 'c': //om vit
 				std::cout << whiteSymbol;
 				break;
-			default:
+			default: //om annat, vilket endast kan betyda fel
 				std::cout << "ERROR";
 				return;
 			}
 		}
-		if (rowNumber == 0) std::cout << "  " << board.numberOfBlackDiscs
+
+		/*till höger om brädet står vissa detaljer. Eftersom dessa skrivs ut
+		på specifika rader, kollar vi här efter rader 1, 2 och 4 (från användarens perspektiv)*/
+		//spelare 1 (svart) - antal brickor och spelartyp
+		if (rowNumber == 0) std::cout << "  " << board.numberOfDiscs[0]
 			<< " - Svart" << (settings.player1iscomp ? " (AI)" : "");
-		else if (rowNumber == 1) std::cout << "  " << board.numberOfWhiteDiscs
+		//spelare 2 (vit)
+		else if (rowNumber == 1) std::cout << "  " << board.numberOfDiscs[1]
 			<< " - Vit" << (settings.player2iscomp ? " (AI)" : "");
+		//vems tur det är
 		else if (rowNumber == 3) std::cout <<
-			(board.isBlacksTurn ? "  *Svarts tur*" : " *Vits tur");
+			(board.isBlacksTurn ? "  *Svarts tur*" : " *Vits tur*");
+		
+		//nu har vi skrivit klart raden och kan gå till nästa
 		std::cout << "\n";
 	}
 	std::cout << "\n    \033[F";
 }
 
-GameCoordinate getPlayerInput() {
+//[5]
+GameCoordinates getValidPlayerInput(Board & board, GameSettings settings) {
 	bool invalidInput; //är inputen "trasig"?
 	std::string inputLine; //spara rå input
 
 	//del av inputen som är bokstaven respektive siffran
 	char letterIn = 0, numberIn = 0; 
 
-	int x,y; //de bearbetade koordinaterna
-
-	//prompta användaren
-	std::cout << "Skriv \"bokstav siffra\" där du vill lägga:\n";
+	GameCoordinates finalInput; //de bearbetade koordinaterna
 
 	do { //vi ska kolla input minst en gång men fler ifall ogiltig input ges
+		
+		//prompta användaren
+		std::cout << "Skriv \"bokstav siffra\" där du vill lägga:\n";
 
 		letterIn = 0; //måste nollställas för att kunna ta bearbeta input
 		invalidInput = false; //innan vi har input får vi anta att den är giltig
@@ -127,21 +249,26 @@ GameCoordinate getPlayerInput() {
 		}
 		
 		//bearbeta inputen till index-form
-		x = tolower(letterIn) - 97; //'a' är 97
-		y = numberIn - 49; //'1' är 49 index börjar på 0
+		finalInput.x = tolower(letterIn) - 97; //'a' är 97
+		finalInput.y = numberIn - 49; //'1' är 49 index börjar på 0
 		
 		//om ogiltig input eller koordinater utanför spelplanen
-		if (invalidInput || x < 0 || x > 7 || y < 0 || y > 7) {
+		if (invalidInput || finalInput.isInValid()) {
 			invalidInput = true;
 
-			//flytta markören två rader up ([F)
-			std::cout << "\033[F\033[F"; 
-			//rensa raden ([2K), efterfråga ny inmatning, gå ner en rad och rensa den
-			std::cout << "\033[2KVar god mata in giltiga värden(ex. \"f3\").\n\033[2K";
+			displayBoard(board, settings); //Skriv ut brädet igen
+			std::cout << "Inmatning måste vara en koordinat på brädet (ex. f2)\n";
+		}
+		//om ogiltigt drag (men giltig input i övrigt)
+		else if (!isValidMove(board, finalInput)) {
+			invalidInput = true;
+
+			displayBoard(board, settings); //Skriv ut brädet igen
+			std::cout << "Du kan tyvärr inte lägga där\n";
 		}
 	} while (invalidInput); //om ogitlig input, hoppa upp igen
 
-	return { x,y }; //returnera bearbetade, gilitga koordinater.
+	return finalInput; //returnera bearbetade, gilitga koordinater.
 }
 
 //[6]
@@ -155,7 +282,7 @@ bool endGame(Board& board) {
 }
 
 //[8]
-GameCoordinate chooseComputerMove(Board& board, std::vector<GameCoordinate>, int computerDifficulty) {
+GameCoordinates chooseComputerMove(Board& board, std::vector<GameCoordinates>, int computerDifficulty) {
 	return { 0,0 };
 }
 
@@ -169,9 +296,9 @@ int main() {
 	settings.player1iscomp = false;
 	settings.player2iscomp = true;
 	while (_getch()) {
-		displayBoard(gameBoard, settings);
-		GameCoordinate coords = getPlayerInput();
-		
+		displayBoard(gameBoard, settings, getListOfPossibleMoves(gameBoard));
+		GameCoordinates coords = getValidPlayerInput(gameBoard, settings);
+		placeDisc(gameBoard, coords);
 	}
 	return 0;
 }
